80fbe458e3facf688b4dc60bb1bc81a144f3b967
kubarozwadowski <officialkubarozwadowski@gmail.com>
2026-04-27 20:04:01 -0400

    boilerplate for csv imports

---
+++ src/storage/CSVGradeImporter.java
@@ -0,0 +1,5 @@
+package storage;
+
+public class CSVGradeImporter {
+    //expect csv in the format student_id, assignment_id, points_earned, submission_date, penalty, notes
+}
---
+++ src/storage/CSVStudentImporter.java
@@ -0,0 +1,5 @@
+package storage;
+
+public class CSVStudentImporter {
+    //expects csv in the form id, name, email, grad_year, status
+}
